633685aa7c904d290cdbba66c0ffe495604b89f6
Trent Douglas <trentdouglas@mymail.mines.edu>
2021-03-24 13:50:08 -0600

    right at the canvas survey

--- app/src/main/java/com/csci448/trentdouglas/lab_9/MainActivity.kt
+++ app/src/main/java/com/csci448/trentdouglas/lab_9/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.csci448.trentdouglas.lab_9
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.csci448.trentdouglas.lab_9.databinding.ActivityMainBinding
+import com.csci448.trentdouglas.lab_9.fragments.LocatrFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -17,6 +19,16 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById((binding).navHostFragment.id) as NavHostFragment
         NavigationUI.setupActionBarWithNavController(this, navHostFragment.navController)
+    }
+
+    // on MainActivity.kt
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == LocatrFragment.REQUEST_LOC_ON) {
+            val locatrFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as LocatrFragment
+            locatrFragment.onActivityResult(requestCode, resultCode, data)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 
 
--- app/src/main/java/com/csci448/trentdouglas/lab_9/fragments/LocatrFragment.kt
+++ app/src/main/java/com/csci448/trentdouglas/lab_9/fragments/LocatrFragment.kt
@@ -1,23 +1,58 @@
 package com.csci448.trentdouglas.lab_9.fragments
 
+import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import com.csci448.trentdouglas.lab_9.R
 import com.csci448.trentdouglas.lab_9.databinding.FragmentLocatrBinding
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
 
 class LocatrFragment: Fragment() {
 
+    companion object{
+        public const val REQUEST_LOC_ON = 0
+        private var locationUpdateState = false
+    }
+    private lateinit var locationRequest: LocationRequest
     private var _binding:FragmentLocatrBinding? = null
-    // This property is only valid between onCreateView and onDestroyView
     private val binding get() = _binding!!
 
     private val LOG_TAG = "locatrFragment: "
     override fun onCreate(savedInstanceState: Bundle?) {
         setHasOptionsMenu(true)
+        locationRequest = LocationRequest.create()
+        locationRequest.interval=0
+        locationRequest.numUpdates=1
+        locationRequest.priority=LocationRequest.PRIORITY_HIGH_ACCURACY
+
         super.onCreate(savedInstanceState)
     }
+
+    private fun checkIfLocationCanBeRetrieved() {
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val client = LocationServices.getSettingsClient(requireActivity())
+        client.checkLocationSettings(builder.build()).apply {
+            addOnSuccessListener {
+                locationUpdateState = true
+                requireActivity().invalidateOptionsMenu()
+            }
+            addOnFailureListener {exc ->
+                locationUpdateState = false
+                requireActivity().invalidateOptionsMenu()
+                if(exc is ResolvableApiException) {
+                    try {
+                        exc.startResolutionForResult(requireActivity(), REQUEST_LOC_ON)
+                    } catch (e: IntentSender.SendIntentException) {
+                        // do nothing, they cancelled so ignore error
+                    }
+                } }
+        } }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Log.d(LOG_TAG, "onCreateView() called")
@@ -29,6 +64,8 @@ class LocatrFragment: Fragment() {
         super.onCreateOptionsMenu(menu, inflater)
         Log.d(LOG_TAG, "onCreateOptionsMenu() called")
         inflater.inflate(R.menu.fragment_locatr, menu)
+        val locationItem = menu.findItem(R.id.get_location_menu_item)
+        locationItem.isEnabled = locationUpdateState
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -36,6 +73,11 @@ class LocatrFragment: Fragment() {
         return super.onOptionsItemSelected(item)
     }
 
+    override fun onStart() {
+        super.onStart()
+        checkIfLocationCanBeRetrieved()
+
+    }
 
 
 }
