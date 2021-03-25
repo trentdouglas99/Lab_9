daeaecc19f3ecac7049a37f8cc74562518500203
trentdouglas99 <trentdouglas@mymail.mines.edu>
2021-03-25 11:00:22 -0600

    rrrr

--- app/src/main/java/com/csci448/trentdouglas/lab_9/fragments/LocatrFragment.kt
+++ app/src/main/java/com/csci448/trentdouglas/lab_9/fragments/LocatrFragment.kt
@@ -1,16 +1,30 @@
 package com.csci448.trentdouglas.lab_9.fragments
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.app.Activity
+import android.content.Intent
 import android.content.IntentSender
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.*
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.csci448.trentdouglas.lab_9.MainActivity
 import com.csci448.trentdouglas.lab_9.R
 import com.csci448.trentdouglas.lab_9.databinding.FragmentLocatrBinding
 import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.*
+import java.io.IOException
 
 class LocatrFragment: Fragment() {
 
@@ -21,16 +35,72 @@ class LocatrFragment: Fragment() {
     private lateinit var locationRequest: LocationRequest
     private var _binding:FragmentLocatrBinding? = null
     private val binding get() = _binding!!
+    private lateinit var locationPermissionCallback: ActivityResultCallback<Boolean>
+    private lateinit var locationPermissionLauncher: ActivityResultLauncher<String>
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
 
     private val LOG_TAG = "locatrFragment: "
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setHasOptionsMenu(true)
         locationRequest = LocationRequest.create()
         locationRequest.interval=0
         locationRequest.numUpdates=1
         locationRequest.priority=LocationRequest.PRIORITY_HIGH_ACCURACY
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                Log.d(LOG_TAG, "Got a location: ${locationResult.lastLocation}")
+                binding.locationTextView.text = "(${locationResult.lastLocation.latitude},${locationResult.lastLocation.longitude})"
+                binding.addressTextView.text = getAddress(locationResult.lastLocation)
+            }
+        }
+
+        locationPermissionCallback = ActivityResultCallback { isGranted: Boolean ->
+            if(isGranted) {
+                // permission is granted
+                Log.d(LOG_TAG, "permission granted!")
+                // make sure button is enabled and launch the pickContact intent
+
+
+            } else {
+                // permission denied, explain to the user
+                Log.d(LOG_TAG, "permission denied, please grant permission")
+            }
+        }
+        locationPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission(), locationPermissionCallback)
+
 
         super.onCreate(savedInstanceState)
+    }
+
+    private fun getAddress(location: Location): String {
+        val geocoder = Geocoder(requireActivity())
+        val addressTextBuilder = StringBuilder()
+        try {
+            val addresses = geocoder.getFromLocation(location.latitude,
+                    location.longitude,
+                    1)
+            if(addresses != null && addresses.isNotEmpty()) {
+                val address = addresses[0]
+                for(i in 0..address.maxAddressLineIndex) {
+                    if(i > 0) {
+                        addressTextBuilder.append( "\n" )
+                    }
+                    addressTextBuilder.append( address.getAddressLine(i) )
+                }
+            }
+        } catch (e: IOException) {
+            Log.e(LOG_TAG, "Error getting address")
+        }
+        return addressTextBuilder.toString()
     }
 
     private fun checkIfLocationCanBeRetrieved() {
@@ -42,7 +112,8 @@ class LocatrFragment: Fragment() {
                 locationUpdateState = true
                 requireActivity().invalidateOptionsMenu()
             }
-            addOnFailureListener {exc ->
+            addOnFailureListener {
+                    exc ->
                 locationUpdateState = false
                 requireActivity().invalidateOptionsMenu()
                 if(exc is ResolvableApiException) {
@@ -52,7 +123,45 @@ class LocatrFragment: Fragment() {
                         // do nothing, they cancelled so ignore error
                     }
                 } }
-        } }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data:
+    Intent?) {
+        if(resultCode != Activity.RESULT_OK) {
+            return
+        }
+        if(requestCode == REQUEST_LOC_ON) {
+            locationUpdateState = true
+            requireActivity().invalidateOptionsMenu()
+        }
+    }
+
+    fun checkPermissionAndGetLocation(){
+        if (ContextCompat.checkSelfPermission( requireContext(), ACCESS_FINE_LOCATION )!= PERMISSION_GRANTED){
+            // permission not granted
+            // check if we should ask
+            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), ACCESS_FINE_LOCATION)) {
+                // user already said no, don't ask again
+                Toast.makeText(requireContext(), "We must access your location to plot where you are", Toast.LENGTH_SHORT).show()
+            }
+            else{
+
+                ContextCompat.checkSelfPermission(requireContext(), ACCESS_FINE_LOCATION )
+                //Launch permission ResultLauncher for ACCESS_FINE_LOCATION_PERMISSION
+                Log.d(LOG_TAG, "requesting permission...")
+                locationPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+            }
+
+        }
+
+        else{
+            // permission has been granted, do what we want
+            Log.d(LOG_TAG, "thanks!")
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+        }
+
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Log.d(LOG_TAG, "onCreateView() called")
@@ -66,9 +175,15 @@ class LocatrFragment: Fragment() {
         inflater.inflate(R.menu.fragment_locatr, menu)
         val locationItem = menu.findItem(R.id.get_location_menu_item)
         locationItem.isEnabled = locationUpdateState
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.get_location_menu_item) {
+            Log.d(LOG_TAG, "checking permissions")
+            checkPermissionAndGetLocation()
+            return true
+        }
         Log.d(LOG_TAG, "called location button!")
         return super.onOptionsItemSelected(item)
     }
@@ -77,6 +192,11 @@ class LocatrFragment: Fragment() {
         super.onStart()
         checkIfLocationCanBeRetrieved()
 
+    }
+
+    override fun onStop() {
+        fusedLocationProviderClient.removeLocationUpdates( locationCallback )
+        super.onStop()
     }
 
 
