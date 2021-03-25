12b7c1ad185ac66a7e19745d34f55e684bb26add
Trent Douglas <trentdouglas@mymail.mines.edu>
2021-03-25 17:26:37 -0600

    done

--- app/src/main/java/com/csci448/trentdouglas/lab_9/fragments/LocatrFragment.kt
+++ app/src/main/java/com/csci448/trentdouglas/lab_9/fragments/LocatrFragment.kt
@@ -24,26 +24,57 @@ import com.csci448.trentdouglas.lab_9.R
 import com.csci448.trentdouglas.lab_9.databinding.FragmentLocatrBinding
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
 import java.io.IOException
+import kotlin.text.StringBuilder
 
-class LocatrFragment: Fragment() {
+class LocatrFragment: SupportMapFragment() {
 
     companion object{
         public const val REQUEST_LOC_ON = 0
         private var locationUpdateState = false
     }
     private lateinit var locationRequest: LocationRequest
-    private var _binding:FragmentLocatrBinding? = null
-    private val binding get() = _binding!!
-    private lateinit var locationPermissionCallback: ActivityResultCallback<Boolean>
+//    private var _binding:FragmentLocatrBinding? = null
+//    private val binding get() = _binding!!
+    //private lateinit var locationPermissionCallback: ActivityResultCallback<Boolean>
     private lateinit var locationPermissionLauncher: ActivityResultLauncher<String>
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var locationCallback: LocationCallback
+    private lateinit var googleMap: GoogleMap
+    private lateinit var lastLocation: Location
 
     private val LOG_TAG = "locatrFragment: "
 
 
-
+    private fun updateUI() {
+        // make sure we have a map and a location
+        if( !::googleMap.isInitialized || !::lastLocation.isInitialized ) {
+            return }
+        // create a point for the corresponding lat/long
+        val myLocationPoint = LatLng(lastLocation.latitude, lastLocation.longitude)
+        // Step 3 will go here
+        // create the marker
+        val myMarker = MarkerOptions().position(myLocationPoint).title( getAddress(lastLocation) )
+        // clear any prior markers on the map
+        googleMap.clear()
+        // add the new markers
+        googleMap.addMarker(myMarker)
+        // include all points that should be within the bounds of the zoom
+        // convex hull
+        val bounds = LatLngBounds.Builder().include(myLocationPoint).build()
+        // add a margin
+        val margin = resources.getDimensionPixelSize(R.dimen.map_inset_margin)
+        // create a camera to smoothly move the map view
+        val cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, margin)
+        // move our camera!
+        googleMap.animateCamera(cameraUpdate)
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,23 +87,34 @@ class LocatrFragment: Fragment() {
 
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult) {
+                lastLocation = locationResult.lastLocation
                 super.onLocationResult(locationResult)
                 Log.d(LOG_TAG, "Got a location: ${locationResult.lastLocation}")
-                binding.locationTextView.text = "(${locationResult.lastLocation.latitude},${locationResult.lastLocation.longitude})"
-                binding.addressTextView.text = getAddress(locationResult.lastLocation)
+ //               binding.locationTextView.text = "(${locationResult.lastLocation.latitude},${locationResult.lastLocation.longitude})"
+ //               binding.addressTextView.text = getAddress(locationResult.lastLocation)
+                updateUI()
             }
         }
 
-        locationPermissionCallback = ActivityResultCallback { isGranted: Boolean ->
-            if(isGranted) {
+        getMapAsync { map ->
+            googleMap = map
+            requireActivity().invalidateOptionsMenu()
+        }
+
+
+
+        var locationPermissionCallback = ActivityResultCallback { result: Boolean ->
+            if(result) {
                 // permission is granted
                 Log.d(LOG_TAG, "permission granted!")
-                // make sure button is enabled and launch the pickContact intent
 
+                // make sure button is enabled and launch the pickContact intent
+                checkIfLocationCanBeRetrieved()
 
             } else {
                 // permission denied, explain to the user
                 Log.d(LOG_TAG, "permission denied, please grant permission")
+
             }
         }
         locationPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission(), locationPermissionCallback)
@@ -98,7 +140,9 @@ class LocatrFragment: Fragment() {
                 }
             }
         } catch (e: IOException) {
-            Log.e(LOG_TAG, "Error getting address")
+            Log.e(LOG_TAG, "Error getting address ${e.localizedMessage}")
+            //binding.addressTextView.text = "error getting location"
+            return "error getting address :("
         }
         return addressTextBuilder.toString()
     }
@@ -139,20 +183,15 @@ class LocatrFragment: Fragment() {
 
     fun checkPermissionAndGetLocation(){
         if (ContextCompat.checkSelfPermission( requireContext(), ACCESS_FINE_LOCATION )!= PERMISSION_GRANTED){
-            // permission not granted
-            // check if we should ask
+
             if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), ACCESS_FINE_LOCATION)) {
                 // user already said no, don't ask again
                 Toast.makeText(requireContext(), "We must access your location to plot where you are", Toast.LENGTH_SHORT).show()
             }
             else{
-
-                ContextCompat.checkSelfPermission(requireContext(), ACCESS_FINE_LOCATION )
-                //Launch permission ResultLauncher for ACCESS_FINE_LOCATION_PERMISSION
                 Log.d(LOG_TAG, "requesting permission...")
                 locationPermissionLauncher.launch(ACCESS_FINE_LOCATION)
             }
-
         }
 
         else{
@@ -160,13 +199,12 @@ class LocatrFragment: Fragment() {
             Log.d(LOG_TAG, "thanks!")
             fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
         }
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Log.d(LOG_TAG, "onCreateView() called")
-        _binding = FragmentLocatrBinding.inflate(inflater, container, false)
-        return binding.root
+        val mapView = super.onCreateView(inflater, container, savedInstanceState)
+        return mapView
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -174,7 +212,8 @@ class LocatrFragment: Fragment() {
         Log.d(LOG_TAG, "onCreateOptionsMenu() called")
         inflater.inflate(R.menu.fragment_locatr, menu)
         val locationItem = menu.findItem(R.id.get_location_menu_item)
-        locationItem.isEnabled = locationUpdateState
+        //locationItem.isEnabled = locationUpdateState
+        locationItem.isEnabled = (locationUpdateState && ::googleMap.isInitialized)
 
     }
 
@@ -195,6 +234,7 @@ class LocatrFragment: Fragment() {
     }
 
     override fun onStop() {
+        Log.d(LOG_TAG, "onStop called")
         fusedLocationProviderClient.removeLocationUpdates( locationCallback )
         super.onStop()
     }
